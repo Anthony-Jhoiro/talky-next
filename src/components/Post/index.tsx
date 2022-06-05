30c81ce64fde7b4e1c7636dbc1d05bfde1161da4
anthony <quereantho@gmail.com>
2022-06-05 11:28:08 +0200

    :lipstick: Setup Tailwind and storybook

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { ProfilePicture } from "../ProfilePicture";
+import { AssetGrid } from "./AssetGrid";
+
+export type AssetType = "IMAGE" | "PDF_FILE" | "OTHER";
+
+export interface Asset {
+  type: AssetType;
+  url: string;
+}
+
+export interface Post {
+  author: {
+    name: string;
+    image: string;
+    id: string;
+  };
+  content: string;
+  assets: Asset[];
+}
+
+export interface PostProps {
+  post: Post;
+}
+
+export const Post: React.VFC<PostProps> = ({ post }: PostProps) => {
+  return (
+    <article className={"shadow-lg p-5"}>
+      <div className={"pb-4"}>
+        <ProfilePicture user={post.author} />
+      </div>
+
+      <div>
+        <p className={"whitespace-pre-line"}>{post.content}</p>
+
+        <AssetGrid assets={post.assets} />
+      </div>
+    </article>
+  );
+};
